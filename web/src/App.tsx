795553818d30110fb34c3d6d310a6e3f7b1d1a5a
Aamir Siddiqui <gt_ams@yahoo.in>
2023-09-04 00:14:34 +0100

    Complete Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,10 @@
-import { MantineProvider } from '@mantine/core'
+import { useState } from 'react'
+
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -16,18 +22,41 @@ import { AuthProvider, useAuth } from './auth'
 
 import './index.css'
 
-const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <AuthProvider>
-        <MantineProvider theme={theme}>
-          <RedwoodApolloProvider useAuth={useAuth}>
-            <Routes />
-          </RedwoodApolloProvider>
-        </MantineProvider>
-      </AuthProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
-)
+const App = () => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(() => {
+    const scheme = localStorage.getItem('mantine-color-scheme')
+    return scheme ? (scheme as ColorScheme) : 'light'
+  })
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+    localStorage.setItem('mantine-color-scheme', colorScheme)
+  }
+
+  return (
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <AuthProvider>
+          <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
+          >
+            <MantineProvider
+              withGlobalStyles
+              withNormalizeCSS
+              theme={{
+                ...theme,
+                colorScheme,
+              }}
+            >
+              <RedwoodApolloProvider useAuth={useAuth}>
+                <Routes />
+              </RedwoodApolloProvider>
+            </MantineProvider>
+          </ColorSchemeProvider>
+        </AuthProvider>
+      </RedwoodProvider>
+    </FatalErrorBoundary>
+  )
+}
 
 export default App
--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -6,7 +6,13 @@ import {
   Text,
   MediaQuery,
   Burger,
+  Group,
+  Image,
+  Title,
+  ActionIcon,
+  useMantineColorScheme,
 } from '@mantine/core'
+import { Sun, MoonStars } from 'tabler-icons-react'
 
 import { useAuth } from 'src/auth'
 import UserCell from 'src/components/UserCell'
@@ -19,6 +25,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   const { isAuthenticated, currentUser } = useAuth()
   const [opened, setOpened] = React.useState(false)
   const theme = useMantineTheme()
+  const { toggleColorScheme } = useMantineColorScheme()
 
   return (
     <AppShell
@@ -36,19 +43,23 @@ const MainLayout = ({ children }: MainLayoutProps) => {
       navbar={
         <Navbar
           width={{ sm: 200, lg: 300 }}
-          height={500}
           p="md"
           hidden={!opened}
           hiddenBreakpoint="sm"
         >
-          <Text>Application</Text>
+          <Navbar.Section grow mt="xs">
+            <Text>Application</Text>
+          </Navbar.Section>
+          <Navbar.Section>
+            {isAuthenticated && currentUser && (
+              <UserCell id={currentUser.sub as string} />
+            )}
+          </Navbar.Section>
         </Navbar>
       }
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
-          <div
-            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
-          >
+          <Group sx={{ height: '100%' }} px={20} position="apart">
             <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
               <Burger
                 opened={opened}
@@ -58,12 +69,27 @@ const MainLayout = ({ children }: MainLayoutProps) => {
                 mr="xl"
               />
             </MediaQuery>
-
-            <Text>Application header</Text>
-            {isAuthenticated && currentUser && (
-              <UserCell id={currentUser.sub as string} />
-            )}
-          </div>
+            <Group
+              spacing={12}
+              sx={{
+                cursor: 'pointer',
+              }}
+            >
+              <Image src="/logo/buying.png" alt="DebtSync" maw={30} mx="auto" />
+              <Title order={3}>DebtSync</Title>
+            </Group>
+            <ActionIcon
+              variant="default"
+              onClick={() => toggleColorScheme()}
+              size={30}
+            >
+              {theme.colorScheme === 'dark' ? (
+                <Sun size="1rem" />
+              ) : (
+                <MoonStars size="1rem" />
+              )}
+            </ActionIcon>
+          </Group>
         </Header>
       }
     >
