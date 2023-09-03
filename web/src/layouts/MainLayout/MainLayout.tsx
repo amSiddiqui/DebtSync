1b42bc21c1c978ee65390550bc33e95f4c4a619f
Aamir Siddiqui <gt_ams@yahoo.in>
2023-09-03 23:31:17 +0100

    responsive Navbar

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,12 @@
-import { AppShell, Header, Navbar } from '@mantine/core'
+import {
+  AppShell,
+  Header,
+  Navbar,
+  useMantineTheme,
+  Text,
+  MediaQuery,
+  Burger,
+} from '@mantine/core'
 
 import { useAuth } from 'src/auth'
 import UserCell from 'src/components/UserCell'
@@ -9,30 +17,55 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   const { isAuthenticated, currentUser } = useAuth()
+  const [opened, setOpened] = React.useState(false)
+  const theme = useMantineTheme()
 
   return (
     <AppShell
       padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
-          Navbar
-        </Navbar>
-      }
-      header={
-        <Header height={60} p="xs">
-          {isAuthenticated && currentUser && (
-            <UserCell id={currentUser.sub as string} />
-          )}
-        </Header>
-      }
-      styles={(theme) => ({
+      styles={{
         main: {
-          backgroundColor:
+          background:
             theme.colorScheme === 'dark'
               ? theme.colors.dark[8]
               : theme.colors.gray[0],
         },
-      })}
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          width={{ sm: 200, lg: 300 }}
+          height={500}
+          p="md"
+          hidden={!opened}
+          hiddenBreakpoint="sm"
+        >
+          <Text>Application</Text>
+        </Navbar>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+            {isAuthenticated && currentUser && (
+              <UserCell id={currentUser.sub as string} />
+            )}
+          </div>
+        </Header>
+      }
     >
       {children}
     </AppShell>
