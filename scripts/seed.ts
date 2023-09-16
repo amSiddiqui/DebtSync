581de0f9fe639eb9e7df3bbd9fa54f5bf8dbf852
Aamir Siddiqui <gt_ams@yahoo.in>
2023-09-16 13:32:23 +0530

    Add seed file

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -3,61 +3,83 @@ import { db } from 'api/src/lib/db'
 
 export default async () => {
   try {
-    //
-    // Manually seed via `yarn rw prisma db seed`
-    // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+    const data: Prisma.UserCreateInput[] = [
+      {
+        id: 'auth0|64f32710e2c29ce3479bef7f',
+        name: 'aamir_siddiqui17@yahoo.com',
+        email: 'john.doe@example.com',
+        accounts: {
+          create: [
+            {
+              name: 'Savings Account',
+              balance: 1000,
+              status: 'active',
+              Transaction: {
+                create: [
+                  {
+                    title: 'Initial Deposit',
+                    description: 'Deposit of $1000',
+                    amount: 1000,
+                    debit: false,
+                  },
+                  {
+                    title: 'Grocery Shopping',
+                    description: 'Bought groceries for $50',
+                    amount: 50,
+                    debit: true,
+                  },
+                  {
+                    title: 'Gas Station',
+                    description: 'Filled up gas for $30',
+                    amount: 30,
+                    debit: true,
+                  },
+                ],
+              },
+            },
+            {
+              name: 'Checking Account',
+              balance: 500,
+              status: 'active',
+              Transaction: {
+                create: [
+                  {
+                    title: 'Initial Deposit',
+                    description: 'Deposit of $500',
+                    amount: 500,
+                    debit: false,
+                  },
+                  {
+                    title: 'Rent Payment',
+                    description: 'Paid rent for $1000',
+                    amount: 1000,
+                    debit: true,
+                  },
+                  {
+                    title: 'Electricity Bill',
+                    description: 'Paid electricity bill for $50',
+                    amount: 50,
+                    debit: true,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
     ]
-    console.log(
-      "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
 
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
+    console.log('Seeding database...')
+
+    await Promise.all(
+      data.map(async (user) => {
+        const record = await db.user.create({ data: user })
+        console.log(`Created user with id: ${record.id}`)
       })
     )
 
-    // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
-    // and associated `salt` to their record. Here's how to create them using
-    // the same algorithm that dbAuth uses internally:
-    //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
-    //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
-    //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
-    //   }
+    console.log('Database seeding complete.')
   } catch (error) {
-    console.warn('Please define your seed data.')
-    console.error(error)
+    console.error('Error seeding database:', error)
   }
 }
