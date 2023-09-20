6245f2c1ecd82921f3eeb557e56246841ab42d3c
Aamir Siddiqui <gt_ams@yahoo.in>
2023-09-20 18:44:15 +0530

    fix broken test cases

--- web/src/components/AddAccount/AddAccount.test.tsx
+++ web/src/components/AddAccount/AddAccount.test.tsx
@@ -1,11 +1,21 @@
 import { render } from '@redwoodjs/testing/web'
 
+import { useAuth } from 'src/auth'
+
 import AddAccount from './AddAccount'
+
+jest.mock('src/auth', () => {
+  return {
+    useAuth: jest.fn(),
+  }
+})
 
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
 
 describe('AddAccount', () => {
+  const userMetadata = { sub: 'SUB_VALUE' }
+  useAuth.mockReturnValue({ userMetadata })
   it('renders successfully', () => {
     expect(() => {
       render(<AddAccount />)
